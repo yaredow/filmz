a0dbb8317ce364c1cbe979d6f77e512d40c9beee
yaredow <yaredyilma11@gmail.com>
2025-07-01 13:22:21 +0300

    remove movie and onboaring route

--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -11,9 +10,6 @@ export default function Index() {
       }}
     >
       <Text className="text-5xl text-dark-200">Welcome!</Text>
-      <Text className="text-5xl justify-center items-center">
-        <Link href="/onboarding">Onboarding</Link>
-      </Text>
     </View>
   );
 }
