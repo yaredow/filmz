25d81cd6d5351b4d61719447235a978523d69690
yaredow <yaredyilma11@gmail.com>
2025-07-02 13:42:11 +0300

    fix: icon importing path

--- apps/native/app/(tabs)/_layout.tsx
+++ apps/native/app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Tabs } from "expo-router";
 import type { ImageSourcePropType } from "react-native";
-import { Image, ImageBackground, Text } from "react-native";
+import { Image, ImageBackground, Text, View } from "react-native";
 import { images } from "@/constants/images";
 
 interface TabIconProps {
@@ -10,20 +10,51 @@ interface TabIconProps {
 }
 
 const TabIcon = ({ focused, name, icon }: TabIconProps) => {
+	if (focused) {
+		return (
+			<ImageBackground
+				source={images.highlight}
+				className="mt-4 flex min-h-16 w-full min-w-[112px] flex-1 flex-row items-center justify-center overflow-hidden rounded-full"
+			>
+				<Image source={icon} tintColor="#151312" className="size-5" />
+				<Text className="ml-2 font-semibold text-base text-secondary">
+					{name}
+				</Text>
+			</ImageBackground>
+		);
+	}
+
 	return (
-		<ImageBackground
-			source={images.highlight}
-			className="mt-4 flex min-h-14 w-full min-w-[112px] flex-1 flex-row items-center justify-center overflow-hidden rounded-full"
-		>
-			<Image source={icon} tintColor="#151312" className="size-5" />
-			<Text className="ml-2 font-semibold text-base text-red-500">{name}</Text>
-		</ImageBackground>
+		<View className="mt-4 size-full items-center justify-center rounded-full">
+			<Image source={icon} tintColor="#a8b5db" className="size-5" />
+		</View>
 	);
 };
 
 const _layout = () => {
 	return (
-		<Tabs>
+		<Tabs
+			screenOptions={{
+				tabBarShowLabel: false,
+				tabBarItemStyle: {
+					width: "100%",
+					height: "100%",
+					justifyContent: "center",
+					alignItems: "center",
+				},
+				tabBarStyle: {
+					backgroundColor: "#0f0d23",
+					borderRadius: 50,
+					marginHorizontal: 20,
+					marginBottom: 36,
+					height: 52,
+					position: "absolute",
+					overflow: "hidden",
+					borderWidth: 1,
+					borderColor: "0f0d23",
+				},
+			}}
+		>
 			<Tabs.Screen
 				name="index"
 				options={{
