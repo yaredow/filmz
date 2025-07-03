a74ab99ae1ad6c4602e1b1f6446a56df27b6ffb9
yaredow <yaredyilma11@gmail.com>
2025-07-03 14:48:43 +0000

    fix error in convex

--- packages/backend/convex/_generated/api.d.ts
+++ packages/backend/convex/_generated/api.d.ts
@@ -13,7 +13,9 @@ import type {
 	FilterApi,
 	FunctionReference,
 } from "convex/server";
+import type * as constants from "../constants.js";
 import type * as healthCheck from "../healthCheck.js";
+import type * as movies from "../movies.js";
 import type * as todos from "../todos.js";
 
 /**
@@ -25,7 +27,9 @@ import type * as todos from "../todos.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+	constants: typeof constants;
 	healthCheck: typeof healthCheck;
+	movies: typeof movies;
 	todos: typeof todos;
 }>;
 export declare const api: FilterApi<
