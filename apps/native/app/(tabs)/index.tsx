94b85cbb7a968855dfebe3b022013fa731541bb6
yaredow <yaredyilma11@gmail.com>
2025-07-05 10:54:15 +0300

    fix type and version mismatches

--- apps/native/app/(tabs)/index.tsx
+++ apps/native/app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "expo-router";
 import { Image, ScrollView, View } from "react-native";
+
 import SearchBar from "@/components/search-bar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
