06979e0f813986976a499a1b728dde0eadb6a69a
yaredow <yaredyilma11@gmail.com>
2025-07-07 10:54:59 +0300

    fix sign up page

--- apps/native/components/auth/sign-up-form.tsx
+++ apps/native/components/auth/sign-up-form.tsx
@@ -8,8 +8,6 @@ const SignUpForm = () => {
 
 	const form = useForm({
 		defaultValues: {
-			firstName: "",
-			lastName: "",
 			email: "",
 			password: "",
 		},
@@ -23,8 +21,6 @@ const SignUpForm = () => {
 				await signUp.create({
 					emailAddress: value.email,
 					password: value.password,
-					firstName: value.firstName,
-					lastName: value.lastName,
 				});
 
 				await signUp.prepareEmailAddressVerification({
@@ -38,50 +34,6 @@ const SignUpForm = () => {
 
 	return (
 		<View className="flex-1 items-center justify-center px-4">
-			<form.Field name="firstName">
-				{(field) => (
-					<>
-						<Text>First Name:</Text>
-						<TextInput
-							value={field.state.value}
-							onChangeText={field.handleChange}
-							autoCapitalize="none"
-							keyboardType="email-address"
-							className="mb-2 w-64 rounded border border-gray-300 p-2"
-						/>
-						{!field.state.meta.isValid && (
-							<Text className="text-red-500">
-								{field.state.meta.errors
-									.map((err) => (typeof err === "string" ? err : err?.message))
-									.join(", ")}
-							</Text>
-						)}
-					</>
-				)}
-			</form.Field>
-
-			<form.Field name="lastName">
-				{(field) => (
-					<>
-						<Text>Last Name:</Text>
-						<TextInput
-							value={field.state.value}
-							onChangeText={field.handleChange}
-							autoCapitalize="none"
-							keyboardType="default"
-							className="mb-2 w-64 rounded border border-gray-300 p-2"
-						/>
-						{!field.state.meta.isValid && (
-							<Text className="text-red-500">
-								{field.state.meta.errors
-									.map((err) => (typeof err === "string" ? err : err?.message))
-									.join(", ")}
-							</Text>
-						)}
-					</>
-				)}
-			</form.Field>
-
 			<form.Field name="email">
 				{(field) => (
 					<>
@@ -131,7 +83,7 @@ const SignUpForm = () => {
 			>
 				{([canSubmit, isSubmitting]) => (
 					<Button
-						title={isSubmitting ? "Signing In..." : "Sign In"}
+						title={isSubmitting ? "Signing Up..." : "Sign Up"}
 						onPress={() => form.handleSubmit()}
 						disabled={!canSubmit || isSubmitting}
 					/>
